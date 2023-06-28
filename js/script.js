939d26a17ecdafab0b1c2f7a74d914f5c4162bf2
SM_artist <48393754+jbrace817@users.noreply.github.com>
2023-06-28 10:49:35 -0400

    Eventlistener and display length set

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 //display
 const displayDiv = document.getElementsByClassName("display")[0];
+displayDiv.style.font;
 let display = 0;
 displayDiv.textContent = display;
 
 //buttons
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    if (display === 0) {
+      displayDiv.textContent = "";
+    }
+    if (displayDiv.textContent.length < 9) {
+      let pressed = e.target.innerText;
+      display = parseFloat(pressed);
+      displayDiv.textContent += display;
+      console.log(typeof parseFloat(display));
+    }
+  })
+);
+
+//Functions for basic math operators
+
+function add(num1, num2) {
+  sum = num1 + num2;
+  return sum;
+}
+function subtract(num1, num2) {
+  sum = num1 - num2;
+  return sum;
+}
+function multiply(num1, num2) {
+  sum = num1 * num2;
+  return sum;
+}
+function divide(num1, num2) {
+  sum = num1 / num2;
+  return sum;
+}
